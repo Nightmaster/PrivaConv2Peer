8f347937074a2c22c15978a15cc6478aa1ce3a5f
Nightmaster <nightmaster@seitaer.com>
2014-03-30 20:24:27 +0200

    MàJ classes vue Signed-off-by: Nightmaster <nightmaster@seitaer.com>

--- src/Test.java
+++ src/Test.java
@@ -5,10 +5,10 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		System.out.println(PasswordUtilities.isStrongEnough("AbC*1234").get(Constants.PW_MAJ));
-		System.out.println(PasswordUtilities.isStrongEnough("abc%1234").get(Constants.PW_MAJ));
-		System.out.println(PasswordUtilities.isStrongEnough("AbC*1234").get(Constants.PW_MIN));
-		System.out.println(PasswordUtilities.isStrongEnough("ABC%1234").get(Constants.PW_MIN));
+		System.out.println(PasswordUtilities.isStrongEnough("AbC*1234").get(Constants.PW_GOOD_FMT));
+		System.out.println(PasswordUtilities.isStrongEnough("abc%1234").get(Constants.PW_GOOD_FMT));
+		System.out.println(PasswordUtilities.isStrongEnough("AbC*1234").get(Constants.PW_GOOD_FMT));
+		System.out.println(PasswordUtilities.isStrongEnough("ABC%1234").get(Constants.PW_GOOD_FMT));
 	}
 
 }
