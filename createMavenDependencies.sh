0c71684442052521f419b66e410faecdf2199fad
Nightmaster <nightmaster@seitaer.com>
2014-10-10 01:58:18 +0200

    Add Gateway dependency Signed-off-by: Nightmaster <nightmaster@seitaer.com>

--- createMavenDependencies.sh
+++ createMavenDependencies.sh
@@ -1,3 +1,4 @@
 cd lib;
 $M2_HOME/bin/mvn install:install-file -Dfile=prompt.jar -DgroupId=org.jdesktop.xswingx -DartifactId=PromptSupport -Dversion=1.0 -Dpackaging=jar;
+$M2_HOME/bin/mvn install:install-file -Dfile=gateway.jar -DgroupId=org.bitlet.weupnp -DartifactId=Gateway -Dversion=1.0 -Dpackaging=jar
 exit;
