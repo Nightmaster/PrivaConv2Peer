b57bb92aee04c0b2701a3eae3523a91390a881ff
Nightmaster <nightmaster@seitaer.com>
2014-10-13 16:52:59 +0200

    Rollback to prompt API instead of the full XSwingX Signed-off-by: Nightmaster <nightmaster@seitaer.com>

--- createMavenDependencies.sh
+++ createMavenDependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd lib;
-$M2_HOME/bin/mvn install:install-file -Dfile=xswingx.jar -DgroupId=org.jdesktop.xswingx -DartifactId=XSwingX -Dversion=1.0 -Dpackaging=jar -Dsources=xswingx-sources.jar;
+$M2_HOME/bin/mvn install:install-file -Dfile=prompt.jar -DgroupId=org.jdesktop.xswingx -DartifactId=XSwingX -Dversion=1.0 -Dpackaging=jar -Dsources=prompt-sources.jar;
 $M2_HOME/bin/mvn install:install-file -Dfile=gateway.jar -DgroupId=org.bitlet.weupnp -DartifactId=Gateway -Dversion=1.0 -Dpackaging=jar
 exit;
